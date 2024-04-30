b9161644c8d71b0475808b9a4869062fc4bc7ad4
권은비 <dev_eunbi@gwon-eunbiui-MacBookPro.local>
2024-04-30 15:48:45 +0900

    feat: 프로젝트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classNames from 'classnames/bind'
+
+import styles from './App.module.scss'
+import { useEffect, useState } from 'react'
+
+const cx = classNames.bind(styles)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  // wedding 데이터 호출
+  useEffect(() => {
+    setLoading(true)
+    fetch('http://localhost:8888/wedding33')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('청첩장 정보를 불러오지 못했습니다.')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setWedding(data)
+        setLoading(false)
+      })
+      .catch((e) => {
+        console.log('에러발생', e)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error...</div>
+  }
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
-export default App;
+export default App
---
+++ src/components/shared/FullScreenMessage.tsx
@@ -0,0 +1,14 @@
+import styles from './FullScreenMessage.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
+
+interface FullScreenMessageProps {
+  type: 'loading' | 'error'
+}
+
+function FullScreenMessage() {
+  return <div className={cx('container')}></div>
+}
+
+export default FullScreenMessage
